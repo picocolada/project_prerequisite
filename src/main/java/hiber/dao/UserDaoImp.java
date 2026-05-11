f42b49ae34e374f6c585f5c25871437f8ff58559
picocolada <deidei322@gmail.com>
2026-05-12 00:22:39 +0300

    orm task 1

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,40 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User findUserByCar(String model, int series) {
+        String hql = "FROM User u WHERE u.car.model = :model AND u.car.series = :series";
+        TypedQuery<User> query = sessionFactory.getCurrentSession()
+                .createQuery(hql, User.class);
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+
+        return query.getSingleResult();
+    }
+
+}
