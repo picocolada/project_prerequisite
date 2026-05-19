cac1de3d762e10fd4e0bdcd0601fa7a5974e628b
picocolada <deidei322@gmail.com>
2026-05-19 16:04:57 +0300

    tomcat task 1

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarServiceImpl;
+
+
+@Controller
+@RequestMapping("/cars")
+public class CarController {
+    private final CarServiceImpl carService;
+
+    public CarController(CarServiceImpl carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public String showCars(@RequestParam(required = false) Integer count, Model model){
+        model.addAttribute("cars", carService.getCars(count));
+        return "cars";
+    }
+}
